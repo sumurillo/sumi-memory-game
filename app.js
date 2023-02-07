ff1e8a0271e0ea4524344be27c6d9dceafa12ae1
sumurillo <sumire.ohana@gmail.com>
2023-02-07 11:33:18 -0600

    shuffle array works

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 /*----- app's state (variables) -----*/
-let game;
 
-let card1 = document.getElementById('r1c1');
-let card2 = document.getElementById('r1c2');
-let card3 = document.getElementById('r1c3');
-let card4 = document.getElementById('r1c4');
-let card5 = document.getElementById('r2c1');
-let card6 = document.getElementById('r2c2');
-let card7 = document.getElementById('r2c3');
-let card8 = document.getElementById('r2c4');
-let card9 = document.getElementById('r3c1');
-let card10 = document.getElementById('r3c2');
-let card11 = document.getElementById('r3c3');
-let card12 = document.getElementById('r3c4');
+const boardEl = document.getElementById('board');
+const startBtn = document.getElementById('button1');
+const resetBtn = document.getElementById('button2');
+let cards = {
+	 card1: document.getElementById('r1c1'),
+	 card2: document.getElementById('r1c2'),
+	 card3: document.getElementById('r1c3'),
+	 card4: document.getElementById('r1c4'),
+	 card5: document.getElementById('r2c1'),
+	 card6: document.getElementById('r2c2'),
+	 card7: document.getElementById('r2c3'),
+	 card8: document.getElementById('r2c4'),
+	 card9: document.getElementById('r3c1'),
+	 card10: document.getElementById('r3c2'),
+	 card11: document.getElementById('r3c3'),
+	 card12: document.getElementById('r3c4')
+};
 
-const gameArr = [
-	[card1, card2, card3, card4],
-	[card5, card6, card7, card8],
-    [card9, card10, card11, card12]
-];
+let choices = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]; //choices to match
 
-let choices = [1, 2, 3, 4, 5, 6];
+
+/*----- variables-----*/
+
+let gameArr = [];
+
 
 /*----- cached element references -----*/
 
 
-// const boardEl = document.getElementById('board');
-const startBtn = document.getElementById('button1');
-const resetBtn = document.getElementById('button2');
 
 
 startBtn.addEventListener('click', countdown);
@@ -35,8 +36,24 @@ startBtn.addEventListener('click', countdown);
 
 /*----- functions -----*/
 
+//shuffle array function
+function shuffle(choices) {
+	let currentIndex = choices.length,  randomIndex;
+  
+	while (currentIndex != 0) {
+	  randomIndex = Math.floor(Math.random() * currentIndex); //math.floor and math.random from w3
+	  currentIndex--;
+  
+	  // And swap it with the current element.
+	  [choices[currentIndex], choices[randomIndex]] = 
+	  [	choices[randomIndex], choices[currentIndex]];
+	}
+	return choices;
+  }
 
-
+	shuffle(choices);
+	gameArr.push(choices);
+	console.log(boardEl);
 
 //timer cited from stack overflow by James McDowell
 timeLeft = 30;
